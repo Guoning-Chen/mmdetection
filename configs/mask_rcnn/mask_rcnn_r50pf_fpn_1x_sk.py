7714a258106446ee1747a0092ec2f2ce02dab011
Guoning Chen <cgn1874@163.com>
2021-06-30 16:30:56 +0800

    resnet_pf运行成功

--- configs/mask_rcnn/mask_rcnn_r50pf_fpn_1x_sk.py
+++ configs/mask_rcnn/mask_rcnn_r50pf_fpn_1x_sk.py
@@ -6,12 +6,7 @@ model = dict(
     backbone=dict(
         type='ResNetPf',
         depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch'),
+        pf_cfg=None),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -8,10 +8,10 @@ from .res2net import Res2Net
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
-from .resnet_pf import ResNetPf, ResNetPfV1d
+from .resnet_pf import ResNetPf
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
     'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'Darknet',
-    'ResNetPf', 'ResNetPfV1d'
+    'ResNetPf'
 ]
